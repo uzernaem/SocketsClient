1a73f8487a5e7421aada9228adc8420fc09bf4ef
Alexander Odintsov <uzernaem@gmail.com>
2021-02-25 17:53:43 +0500

    Add project files.

---
+++ SocketsClient/Client.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Sockets
+{
+    public partial class frmMain : Form
+    {
+        private TcpClient Client = new TcpClient();     // клиентский сокет
+        private UdpClient UClient = new UdpClient();
+        Random rnd = new Random();
+        private IPAddress IP;                           // IP-адрес клиента
+        private Socket ClientSock;
+        private TcpListener Listener;                   // сокет сервера
+        private List<Thread> Threads = new List<Thread>();      // список потоков приложения (кроме родительского)
+        private bool _continue = true;                          // флаг, указывающий продолжается ли работа с сокетами
+
+        // конструктор формы
+        public frmMain()
+        {
+            InitializeComponent();
+
+            tbLogin.Text += rnd.Next().ToString();
+
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());    // информация об IP-адресах и имени машины, на которой запущено приложение
+            IP = hostEntry.AddressList[0];                                  // IP-адрес, который будет указан в заголовке окна для идентификации клиента
+            int Port = 1010;                                                // порт, который будет указан при создании сокета
+
+            // определяем IP-адрес машины в формате IPv4
+            foreach (IPAddress address in hostEntry.AddressList)
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    IP = address;
+                    break;
+                }
+
+            this.Text += "     " + IP.ToString();                           // выводим IP-адрес текущей машины в заголовок формы
+
+            // создаем серверный сокет (Listener для приема заявок от клиентских сокетов)
+            Listener = new TcpListener(IP, 1011);
+            Listener.Start();
+
+            // создаем и запускаем поток, выполняющий обслуживание серверного сокета
+            Threads.Clear();
+            Threads.Add(new Thread(ReceiveMessage));
+            Threads[Threads.Count - 1].Start();
+        }
+
+        private void ReceiveMessage()
+        {
+            // входим в бесконечный цикл для работы с клиентскими сокетом
+            while (_continue)
+            {
+                ClientSock = Listener.AcceptSocket();           // получаем ссылку на очередной клиентский сокет
+                Threads.Add(new Thread(ReadMessages));          // создаем и запускаем поток, обслуживающий конкретный клиентский сокет
+                Threads[Threads.Count - 1].Start(ClientSock);
+            }
+        }
+
+        private void ReadMessages(object ClientSock)
+        {
+            string msg = "";        // полученное сообщение
+
+            // входим в бесконечный цикл для работы с клиентским сокетом
+            while (_continue)
+            {
+                byte[] buff = new byte[1024];                           // буфер прочитанных из сокета байтов
+                ((Socket)ClientSock).Receive(buff);                     // получаем последовательность байтов из сокета в буфер buff
+
+                msg = Encoding.Unicode.GetString(buff);     // выполняем преобразование байтов в последовательность символов
+
+                rtbMessages.Invoke((MethodInvoker)delegate
+                {
+                    if (msg.Replace("\0", "") != "")
+                        rtbMessages.Text += "\n >> " + msg;             // выводим полученное сообщение на форму
+                });
+                Thread.Sleep(500);
+            }
+        }
+
+        // подключение к серверному сокету
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var RequestData = Encoding.ASCII.GetBytes("SomeRequestData");
+                var ServerEp = new IPEndPoint(IPAddress.Any, 0);
+
+                UClient.EnableBroadcast = true;
+                UClient.Send(RequestData, RequestData.Length, new IPEndPoint(IPAddress.Broadcast, 8888));
+
+                var ServerResponseData = UClient.Receive(ref ServerEp);
+                var ServerResponse = Encoding.ASCII.GetString(ServerResponseData);
+                //Console.WriteLine("Recived {0} from {1}", ServerResponse, ServerEp.Address.ToString());
+                tbIP.Text = "Найден сервер " + ServerEp.Address.ToString();
+                UClient.Close();
+
+                int Port = 1010;                                // номер порта, через который выполняется обмен сообщениями
+                IPAddress IP = ServerEp.Address;            // разбор IP-адреса сервера, указанного в поле tbIP
+                Client.Connect(IP, Port);                       // подключение к серверному сокету
+                btnConnect.Enabled = false;
+                btnSend.Enabled = true;
+                tbLogin.Enabled = false;
+                byte[] buff = Encoding.Unicode.GetBytes(tbLogin.Text);   // выполняем преобразование сообщения (вместе с идентификатором машины) в последовательность байт
+                Stream stm = Client.GetStream();                                                    // получаем файловый поток клиентского сокета
+                stm.Write(buff, 0, buff.Length);
+            }
+            catch
+            {
+                MessageBox.Show("Введен некорректный IP-адрес");
+            }
+        }
+
+        // отправка сообщения
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            byte[] buff = Encoding.Unicode.GetBytes(tbLogin.Text + " >> " + tbMessage.Text);   // выполняем преобразование сообщения (вместе с идентификатором машины) в последовательность байт
+            Stream stm = Client.GetStream();                                                    // получаем файловый поток клиентского сокета
+            stm.Write(buff, 0, buff.Length);
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            byte[] buff = Encoding.Unicode.GetBytes(tbLogin.Text + "_logout");   // выполняем преобразование сообщения (вместе с идентификатором машины) в последовательность байт
+            Stream stm = Client.GetStream();                                                    // получаем файловый поток клиентского сокета
+            stm.Write(buff, 0, buff.Length);
+
+            Client.Close();         // закрытие клиентского сокета
+
+            _continue = false;      // сообщаем, что работа с сокетами завершена
+
+            // завершаем все потоки
+            foreach (Thread t in Threads)
+            {
+                t.Abort();
+                t.Join(500);
+            }
+
+            // закрываем клиентский сокет
+            if (ClientSock != null)
+                ClientSock.Close();
+
+            // приостанавливаем "прослушивание" серверного сокета
+            if (Listener != null)
+                Listener.Stop();
+        }
+    }
+}
---
+++ SocketsClient/Client.designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Sockets
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbMessages = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(262, 233);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(91, 26);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(74, 237);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(181, 20);
+            this.tbMessage.TabIndex = 1;
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(3, 187);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(49, 26);
+            this.lblIP.TabIndex = 2;
+            this.lblIP.Text = "IP\r\nсервера";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(262, 180);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(91, 26);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Подключиться";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(3, 240);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(65, 13);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Сообщение";
+            // 
+            // tbIP
+            // 
+            this.tbIP.Enabled = false;
+            this.tbIP.Location = new System.Drawing.Point(74, 184);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(181, 20);
+            this.tbIP.TabIndex = 0;
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(74, 211);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(181, 20);
+            this.tbLogin.TabIndex = 4;
+            this.tbLogin.Text = "user_";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Логин";
+            // 
+            // rtbMessages
+            // 
+            this.rtbMessages.Location = new System.Drawing.Point(7, 12);
+            this.rtbMessages.Name = "rtbMessages";
+            this.rtbMessages.Size = new System.Drawing.Size(347, 166);
+            this.rtbMessages.TabIndex = 6;
+            this.rtbMessages.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 268);
+            this.Controls.Add(this.rtbMessages);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.tbIP);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Клиент";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbMessages;
+    }
+}
+
